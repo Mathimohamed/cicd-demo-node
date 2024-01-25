7205d04436c6a25026369a4b9bd5f012520d821d
Mathimohamed <mathimohamed@gmail.com>
2024-01-25 11:04:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.json({
+    
+        message: "hello welcome to trichy!!"
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server running on port 3000')
+})
