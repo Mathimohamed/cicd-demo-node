9c5f78d7535af7378fffddb916954876316eb028
Mathimohamed <mathimohamed@gmail.com>
2024-01-31 22:57:16 +0530

    Merge remote changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
     
-        message: "hello welcome to trichy!!"
+        message: "hello welcome to tiruchirapalli !!"
     })
 })
 
